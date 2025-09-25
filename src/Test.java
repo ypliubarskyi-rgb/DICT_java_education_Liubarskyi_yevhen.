aef035a31fbe3dec325ad6e146b9ffaba123b5c4
ypliubarskyi-rgb <y.p.liubarskyi@student.khai.edu>
2025-09-25 11:42:04 +0300

     This is a combination of 2 commits.  This is the 1st commit message:
    Test.java added to feature brach
    
     The commit message #2 will be skipped:
    
     added one more print to Test.java on the feature branch
    
    delet file
    
    added a comment to Test.java on the feature branch

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,11 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Java");
+        System.out.println("education");
+        //comment
+        System.out.println("DICT");
     }
+
+
 }
+
+
